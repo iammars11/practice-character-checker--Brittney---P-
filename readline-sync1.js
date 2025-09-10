fd4aa672145e263624a0e820a8e2529df362a419
onmars <mcellab93@gmail.com>
2025-09-09 19:37:37 -0500

    Updated Assignments

--- readline-sync1.js
+++ readline-sync1.js
@@ -1,8 +1,9 @@
 const readlineSync = require("readline-sync");
 let userInput = readlineSync.question("Enter a word or phrase: ");
 let index = readlineSync.questionInt("Enter an index number to find the character: ");
+let character = userInput[index];
 
-console.log("The character at index " + index + " is: " + userInput[index]);
+console.log("The character at index " + index + " is: " + character);
 
 // Output:
 // Enter a word or phrase: Hola
